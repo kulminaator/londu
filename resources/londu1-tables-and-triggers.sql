936e065c950444c0d2488b5d07e5f65509402a60
Martin Roos <kulminaator@gmail.com>
2018-09-04 20:00:23 +0300

    relocating db init sql to resources, dropped it from test db init script, moving db resources to a designated schema to make the world simpler and cleaner

--- resources/londu1-tables-and-triggers.sql
+++ resources/londu1-tables-and-triggers.sql
@@ -1,9 +1,10 @@
 CREATE OR REPLACE LANGUAGE plpgsql;
 
-DROP TABLE IF EXISTS __londu_1_events ;
+CREATE SCHEMA IF NOT EXISTS __londu_1;
+DROP TABLE IF EXISTS __londu_1.events ;
 
 -- create events table
-CREATE TABLE __londu_1_events (
+CREATE TABLE __londu_1.events (
   id BIGSERIAL PRIMARY KEY ,
   tick BIGINT, -- tick id
   tid BIGINT, -- transaction id
@@ -15,25 +16,25 @@ CREATE TABLE __londu_1_events (
 );
 
 -- create the status table on the target, this is where we write down 'where we are' when we sync data over
-DROP TABLE IF EXISTS __londu_1_states;
-CREATE TABLE __londu_1_states(
+DROP TABLE IF EXISTS __londu_1.states;
+CREATE TABLE __londu_1.states(
   id BIGSERIAL PRIMARY KEY,
   event_id BIGINT
 );
 
-CREATE OR REPLACE FUNCTION __londu_1_latest_tick() RETURNS bigint AS $$
+CREATE OR REPLACE FUNCTION __londu_1.latest_tick() RETURNS bigint AS $$
 DECLARE
   current_tick bigint := NULL;
 BEGIN
   -- this lock will be held until our "outer most" transaction commits or rolls back.
-  SELECT ltm.id INTO current_tick FROM __londu_1_ticks ltm
-    WHERE id=(SELECT MAX(id) FROM __londu_1_ticks lts WHERE lts.tid < txid_current()) FOR SHARE;
+  SELECT ltm.id INTO current_tick FROM __londu_1.ticks ltm
+    WHERE id=(SELECT MAX(id) FROM __londu_1.ticks lts WHERE lts.tid < txid_current()) FOR SHARE;
   RETURN current_tick;
 END;
 $$ LANGUAGE plpgsql;
 
 -- create trigger func
-CREATE OR REPLACE FUNCTION __londu_1_trigger() RETURNS trigger AS $londu_1_trigger_function$
+CREATE OR REPLACE FUNCTION __londu_1.trigger() RETURNS trigger AS $londu_1_trigger_function$
 DECLARE
   old_data json := NULL;
   new_data json := NULL;
@@ -45,8 +46,8 @@ BEGIN
   IF TG_OP IN ('INSERT', 'UPDATE') THEN
     new_data = to_json(NEW);
   END IF;
-  current_tick := __londu_1_latest_tick();
-  INSERT INTO __londu_1_events (tid, tick, s, t, op, od, nd)
+  current_tick := __londu_1.latest_tick();
+  INSERT INTO __londu_1.events (tid, tick, s, t, op, od, nd)
     VALUES (txid_current(), current_tick, TG_TABLE_SCHEMA, TG_TABLE_NAME , TG_OP, old_data, new_data);
   IF TG_OP IN ('INSERT', 'UPDATE') THEN
     RETURN NEW;
@@ -60,20 +61,20 @@ $londu_1_trigger_function$ LANGUAGE plpgsql;
 
 -- actually not anymore since june 2018, our code can do this now
 -- recreate triggers
--- DROP TRIGGER IF EXISTS __londu_1_trigger_shop_workers ON shop_workers;
--- CREATE TRIGGER __londu_1_trigger_shop_workers BEFORE INSERT OR UPDATE OR DELETE ON shop_workers
---   FOR EACH ROW EXECUTE PROCEDURE __londu_1_trigger();
+-- DROP TRIGGER IF EXISTS __londu_1.trigger_shop_workers ON shop_workers;
+-- CREATE TRIGGER __londu_1.trigger_shop_workers BEFORE INSERT OR UPDATE OR DELETE ON shop_workers
+--   FOR EACH ROW EXECUTE PROCEDURE __londu_1.trigger();
 
--- DROP TRIGGER IF EXISTS __londu_1_trigger_shop_items ON shop_items;
--- CREATE TRIGGER __londu_1_trigger_shop_items BEFORE INSERT OR UPDATE OR DELETE ON shop_items
---  FOR EACH ROW EXECUTE PROCEDURE __londu_1_trigger();
+-- DROP TRIGGER IF EXISTS __londu_1.trigger_shop_items ON shop_items;
+-- CREATE TRIGGER __londu_1.trigger_shop_items BEFORE INSERT OR UPDATE OR DELETE ON shop_items
+--  FOR EACH ROW EXECUTE PROCEDURE __londu_1.trigger();
 
 -- create ticks table
-DROP TABLE IF EXISTS __londu_1_ticks;
-CREATE TABLE __londu_1_ticks (
+DROP TABLE IF EXISTS __londu_1.ticks;
+CREATE TABLE __londu_1.ticks (
   id BIGSERIAL PRIMARY KEY ,
   tid BIGINT DEFAULT txid_current(),
   created_at TIMESTAMPTZ DEFAULT NOW()
 );
 
-INSERT INTO __londu_1_ticks(created_at) VALUES(default);
+INSERT INTO __londu_1.ticks(created_at) VALUES(default);
--- test/test-db/init_db.sh
+++ test/test-db/init_db.sh
@@ -29,7 +29,7 @@ psql -h 127.0.0.1 -p $DB_PORT londu1_test_source_db -f create-item-row.sql -U sh
 psql -h 127.0.0.1 -p $DB_PORT londu1_test_source_db -f create-item-row.sql -U shopdb_user --no-password
 
 
-echo '-- adding londu to databases '
-export PGPASSFILE=test_pgpass
-psql -h 127.0.0.1 -p $DB_PORT londu1_test_source_db -f londu1-tables-and-triggers.sql -U shopdb_user --no-password
-psql -h 127.0.0.1 -p $DB_PORT londu1_test_target_db -f londu1-tables-and-triggers.sql -U shopdb_user --no-password
+#echo '-- adding londu to databases '
+#export PGPASSFILE=test_pgpass
+#psql -h 127.0.0.1 -p $DB_PORT londu1_test_source_db -f londu1-tables-and-triggers.sql -U shopdb_user --no-password
+#psql -h 127.0.0.1 -p $DB_PORT londu1_test_target_db -f londu1-tables-and-triggers.sql -U shopdb_user --no-password
