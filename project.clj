f9fc351930e2fe32e118ba20adc933c128201d93
Martin Roos <kulminaator@gmail.com>
2018-11-26 21:49:58 +0200

    bump postgres dep version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/java.jdbc "0.7.8"]
-                 [org.postgresql/postgresql "42.2.1"]
+                 [org.postgresql/postgresql "42.2.5"]
                  [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot londu1.core
   :target-path "target/%s"
