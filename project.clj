41565232c74bef43b32c91e43d3b138e287ac1c4
Martin Roos <kulminaator@gmail.com>
2018-12-02 13:44:16 +0200

    enable warnings on reflections

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [org.clojure/java.jdbc "0.7.8"]
                  [org.postgresql/postgresql "42.2.5"]
                  [org.clojure/data.json "0.2.6"]]
+  :global-vars {*warn-on-reflection* true}
   :main ^:skip-aot londu1.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
